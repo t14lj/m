f6721557b72ab1a8a50f91efd84e50daec292ef7
t14lj <s8265242@rg.moe.gov.sa>
2024-05-16 23:11:24 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+player = 0 game.create_sprite(4, 2)
+
+def on_button_pressed_a():
+    pass
+    my_sprite.move(-1)
+input.on_button_pressed(Button.A, on_button_pressed_a)
---
+++ main.ts
@@ -0,0 +1,1 @@
+let player = 0
